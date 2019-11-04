8ebefca282cbea1925299bd4cf27623f05ffef23
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-11-04 16:45:42 -0500

    remove survivors scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_043223) do
+ActiveRecord::Schema.define(version: 2019_10_24_223300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "survivors", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
